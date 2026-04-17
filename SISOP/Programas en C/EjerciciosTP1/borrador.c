94e83d674f1f06e518d15c329ee6f6bedd35189e
Alexander <acruz@fi.uba.ar>
2026-04-17 15:49:19 -0300

    Update: Electromagnetismo and SISOP

--- SISOP/Programas en C/EjerciciosTP1/borrador.c
+++ SISOP/Programas en C/EjerciciosTP1/borrador.c
@@ -1,22 +1,5 @@
-/**Vamos a realizar un nuevo ejercico con malloc and realloc 
- * Que es lo que hace cada uno ?
- * Malloc reserva memoria que lo almacena en el HEAP
- * y realloc lo unico que hace es reasignar(ampliao reduce)
+/**Vamos a ver un par de ejemplo de como podremos trabajar con el uso de memoria inlucyendo 
+ * haciendo un uso muy especial de malloc despues de ello veremos como es el uso de realloc 
+ * and free
  */
 
-
- #include<stdio.h>
- #include<stdlib.h>
-
- int main(){
-
-    int *numeros = malloc(2*sizeof(int));
-
-    numeros[0]=23;
-    numeros[1]=56;
-
-    printf("Los numeros son los siguientes: %d %d\n",numeros[0],numeros[1]);
-    
-
-    return 0;
- }
