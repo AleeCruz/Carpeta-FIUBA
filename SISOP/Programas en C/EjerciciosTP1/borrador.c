80395dac73969e34663325efb057b28c608ff9ad
Alexander <acruz@fi.uba.ar>
2026-04-19 13:24:29 -0300

    Update: Paradigmas de programacion && SIsop

--- SISOP/Programas en C/EjerciciosTP1/borrador.c
+++ SISOP/Programas en C/EjerciciosTP1/borrador.c
@@ -1,36 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/*Vamos a ver un ejemplo sencillo de realloc que nos permita analizar la reasignacion de memoria del heap*/
 
-char *expand_environ_var(char *arg) {
-    // 1. Si NO empieza con '$', devolver arg sin cambios
-        if(arg[0] != '$'){
-            return arg;
-        }
-    // 2. Saltar el '$' para obtener el nombre de la variable
-    //    Pista: arg + 1
-    // 3. Usar getenv() para obtener el valor
-        char* string = getenv(arg+1);
-    // 4. Si no existe o es vacía, devolver NULL
-        if(string ==NULL){
-            return string;
-        }
+/*Que es exactamente realloc?, es una funcion que lo unico que permite aceptar en sus parametros, el 
+puntero original, y ademas reasignar el tamaño  de tu nuevo bloque de memoria*/
 
-    // 5. Copiar el valor con calloc + strcpy y devolverlo
-        char *cadena = (char*)malloc((strlen(string)+1)*sizeof(char));
-        strcpy(cadena,string);
-        return cadena;
-}
 
-int main() {
-    char *casos[] = {"$HOME", "echo", "$NOEXISTE", "$USER", NULL};
 
-    for (int i = 0; casos[i] != NULL; i++) {
-        char *resultado = expand_environ_var(casos[i]);
-        if (resultado == NULL)
-            printf("NULL\n");
-        else
-            printf("%s\n", resultado);
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    //Lo unico que estamos haciendo es asignar 2 bloques de memoria de manera dinamica a un segmento del heap
+    int *numeros;
+    numeros = (int*)malloc(2*sizeof(int));
+
+    //Podemos asignarle un valor a esos bloques de memoria
+    numeros[0] = 12;
+    numeros[1] = 84;
+
+    printf("El valor numerico es de: %d\n",numeros[0]);
+    printf("El valor numerico es de: %d\n",numeros[1]);
+
+
+
+    //Vamos a expandir ese bloque de memoria de la siguiente manera 
+    //Necesitamos asignar un nuevo bloque de memoria que nos permita expandir ese vector dinamico
+
+    int *temporal = (int*)realloc(numeros,4*sizeof(int)); 
+
+    if(temporal != NULL){
+        numeros = temporal;
+        numeros[2] = 23;
+        numeros[3] = 34;   
     }
+
+
+
+    for(int i=0;i<4;i++){
+        printf("tenemos entonces: %d\n",numeros[i]);    
+    }
+    
+    
+    
+
+    //No olvide hacer el free siempre por cada bloque de mallo se debe de realizar unbloque de free
+    free(numeros);
+
     return 0;
 }
